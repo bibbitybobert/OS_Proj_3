227a4f5bbfc22a855ca298f8b575768b51867321
Robert <robdbook@gmail.com>
2024-04-25 16:07:53 -0600

    IT WORKS IM A GENIUS

--- sfs_code/sharedFuncs.c
+++ sfs_code/sharedFuncs.c
@@ -17,15 +17,15 @@ void get_file_block(sfs_inode_t n, uint32_t blk_num, char* data){
         driver_read(data, ptrs[tmp]);
     }
     else if(blk_num < (5 + 32 + (32*32) + (32*32*32))){
-        driver_read(ptrs, n.tindirect);
+        driver_read(ptrs, n.tindirect); //sing ind ptr
         int id_of_blk = blk_num-5-32-(32*32);
         int tmp = id_of_blk/(32 * 32);
-        driver_read(ptrs, ptrs[tmp]);//double indirect now
+        driver_read(ptrs, ptrs[tmp]); //double indirect ptr
         id_of_blk = id_of_blk - (tmp * (32*32));
         tmp = id_of_blk / 32;
-        driver_read(ptrs, ptrs[tmp]);
-        tmp = tmp % 32;
-        driver_read(data, ptrs[tmp]);
+        driver_read(ptrs, ptrs[tmp]); //trip ind ptr list
+        tmp = id_of_blk % 32;
+        driver_read(data, ptrs[tmp]); //id in trip
     }
     else{
         printf("error reading in data\n");
